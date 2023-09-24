e8307a6ee1b8faa2b693b641991cfd8df4edd7df
VriaA <oyelolaabimbola23@gmail.com>
2023-09-24 04:21:46 +0100

    add comments

--- utils/loader.js
+++ utils/loader.js
@@ -1,11 +1,13 @@
 const loader = document.getElementById('loader')
 
+//RENDERS LOADER
 function renderLoader(isFavoriteClicked) {
     if(!isFavoriteClicked) {
         loader.style.display = 'block'
     }
 }
 
+//HIDES LOADER
 function hideLoader() {
     setTimeout(_=> {
         loader.style.display = 'none'
