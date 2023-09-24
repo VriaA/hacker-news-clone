7636d6741c1bd80cdce210136f454f5286628ad1
VriaA <oyelolaabimbola23@gmail.com>
2023-09-24 02:02:13 +0100

    add loader to stories page

--- pages/stories.js
+++ pages/stories.js
@@ -1,3 +1,4 @@
+import loader from "../utils/loader.js"
 import Story from "../components/story.js"
 import Job from "../components/job.js"
 import baseUrl from "../utils/baseUrl.js"
@@ -8,7 +9,6 @@ import store  from "../store.js";
 
 //RENDERS STORIES WHEN CALLED
 async function Stories(path) {
-
     const {favorites} = store.getState()
     const isFavoritesPage = path === '/favorites'
     const isJobPage = path === '/jobs'
@@ -16,27 +16,36 @@ async function Stories(path) {
     let hasAnError = false
 
     try {
+        view.innerHTML = ''
+        loader.style.display = 'block'
         const stories = isFavoritesPage ? favorites : await getStories(path)
         const hasStories = stories.length > 0
 
-        view.innerHTML =  `<div>
-                                ${hasStories ? stories.map((story, i)=> {
-                                    if (isJobPage) {
-                                        return Job({...story, index: i + 1})
-                                    } else {
-                                        return Story({...story, index: i + 1, isFavorite: isInFavorites(favorites, story)}, path)
-                                    } 
-                                } ).join('') : !hasStories && isFavoritesPage ? `<p class='no-result'>No favorites</p>` : `<p class='no-result'>No stories</p>` }
-                            </div>`
-        handleFavoriteClick(path)
+        setTimeout(_=> {
+            view.innerHTML =  `<div>
+                                    ${hasStories ? stories.map((story, i)=> {
+                                        if (isJobPage) {
+                                            return Job({...story, index: i + 1})
+                                        } else {
+                                            return Story({...story, index: i + 1, isFavorite: isInFavorites(favorites, story)}, path)
+                                        } 
+                                    } ).join('') : !hasStories && isFavoritesPage ? `<p class='no-result'>No favorites</p>` : `<p class='no-result'>No stories</p>` }
+                                </div>`
+            handleFavoriteClick(path)
+        }, 1000)
+
     } catch(error) {
         hasAnError = true
     }
 
     if(hasAnError) {
-        view.innerHTML = `<p class="no-result error"> Error fetching stories.<br> Please refresh the page.</p>`
+        view.innerHTML = `<p class="no-result error"> Error fetching stories, please refresh the page.</p>`
     }
 
+    setTimeout(_=> {
+        loader.style.display = 'none'
+    }, 1050)
+    
 }
 
 //GETS STORIES RELATED TO THE CURRENT PATH USING AN API
