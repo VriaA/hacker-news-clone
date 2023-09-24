ebc0b487d98ed0a4840277b88374b5393e978f00
VriaA <oyelolaabimbola23@gmail.com>
2023-09-24 02:45:48 +0100

    refactor loader.js

--- utils/loader.js
+++ utils/loader.js
@@ -1,1 +1,13 @@
-export default document.getElementById('loader')
+const loader = document.getElementById('loader')
+
+function renderLoader() {
+        loader.style.display = 'block'
+}
+
+function hideLoader() {
+    setTimeout(_=> {
+        loader.style.display = 'none'
+    }, 1050)
+}
+
+export {renderLoader, hideLoader}
